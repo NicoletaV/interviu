78f00421b60d3ffa91fd281e47cb1d239eac18e9
NicoletaV <nicoleta.vina@yahoo.com>
2020-06-07 23:05:51 +0300

    Task 2 done - editable quantity; description on hover; total price and final total; remove from cart

--- src/app/components/checkout-page/checkout-page.component.ts
+++ src/app/components/checkout-page/checkout-page.component.ts
@@ -11,6 +11,9 @@ export class CheckoutPageComponent implements OnInit {
   constructor(private productsService: ProductsService) { }
 
   products: any = [];
+  checkout_products: any = [];
+  cart_products: any = [];
+  total_price: any = 0;
 
   ngOnInit(): void {
     this.getProducts();
@@ -20,12 +23,53 @@ export class CheckoutPageComponent implements OnInit {
     this.productsService.getProducts().toPromise()
       .then((data) => {
         this.products = data;
-        this.products.sort((a, b) => (a.price < b.price) ? 1 : ((a.price > b.price) ? -1 : 0));
-        console.log(this.products)
+        this.products.sort(this.compareByPrice);
+        this.checkout_products = this.products;
+        this.cart_products = [];
       })
       .catch((err) => {
         console.log(err);
       })
+  }
+
+  compareByPrice(a, b) {
+    if (a.price < b.price) {
+      return 1;
+    }
+
+    if (a.price > b.price) {
+      return -1;
+    }
+
+    return 0;
+  }
+
+  addToCart(index) {
+    this.cart_products.push(this.checkout_products[index]);
+
+    let index_last_elem = this.cart_products.length - 1;
+    this.cart_products[index_last_elem].quantity = 1;
+    this.cart_products[index_last_elem].total = this.cart_products[index_last_elem].price;
+    this.total_price += this.cart_products[index_last_elem].total;
+
+    this.checkout_products.splice(index, 1);
+  }
+
+  deleteFromCart(index) {
+    this.checkout_products.push(this.cart_products[index]);
+    this.checkout_products.sort(this.compareByPrice);
+
+    this.total_price -= this.cart_products[index].total;
+    this.cart_products.splice(index, 1);
+  }
+
+  updateQuantity($event, index) {
+    if ($event.value > 0) {
+      this.cart_products[index].quantity = $event.value;
+      this.total_price -= this.cart_products[index].total;
+      this.cart_products[index].total = this.cart_products[index].quantity * this.cart_products[index].price;
+      this.total_price += this.cart_products[index].total;
+    }
   }
 
 }
